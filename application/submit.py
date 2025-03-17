eb150f8752643ce84b5130cd3843cfbbb8f9e051
seal <3291351273@qq.com>
2025-03-17 13:39:56 +0800

    情况判定

--- application/submit.py
+++ application/submit.py
@@ -19,12 +19,28 @@ def submit_answers():
     # 假设前端传来的日期已经精确到秒，这里直接使用
     date = submission_data.get('date')
 
+    # 根据分数评判认知功能
+    if 270 <= score <= 300:
+        cognitive_function = "正常"
+    elif score < 270:
+        if 210 <= score <= 260:
+            cognitive_function = "轻度"
+        elif 100 <= score <= 200:
+            cognitive_function = "中度"
+        elif 0 <= score <= 90:
+            cognitive_function = "重度"
+        else:
+            cognitive_function = "未知"
+    else:
+        cognitive_function = "未知"
+
     # 构建要保存的单次提交数据（老数据）
     submission = {
         "answerRecords": answerRecords,
         "answerResults": answerResults,
         "score": score,
-        "date": date
+        "date": date,
+        "认知功能": cognitive_function
     }
 
     try:
@@ -51,29 +67,27 @@ def submit_answers():
                 "username": user,
                 "data": {}
             }
-        deliver_score_datas=deliver_score_data.get('data', {})
+        deliver_score_datas = deliver_score_data.get('data', {})
         # 获取该用户所有训练类型的最大id
-        max_id=0
+        max_id = 0
         for training_type in deliver_score_datas:
             for record in deliver_score_datas[training_type]:
-                
-                if record["date"][:10]==date[:10]:
-                    max_id=max(int(record["id"]),max_id)
+                if record["date"][:10] == date[:10]:
+                    max_id = max(int(record["id"]), max_id)
         new_id = max_id + 1
         # 这里需要确保date包含秒信息，假设前端传来的格式是'YYYY/MM/DD HH:MM:SS'
         current_date = datetime.strptime(date, '%Y/%m/%d %H:%M:%S')
 
-        
-        value={
-            "失算症训练": { "data": 0 },
-            "思维障碍训练": { "data": 0 },
-            "注意障碍训练": { "data": 0 },
-            "知觉障碍训练": { "data": 0 },
-            "记忆障碍训练": { "data": 0 }
+        value = {
+            "失算症训练": {"data": 0},
+            "思维障碍训练": {"data": 0},
+            "注意障碍训练": {"data": 0},
+            "知觉障碍训练": {"data": 0},
+            "记忆障碍训练": {"data": 0}
         }
         for training_type in training_data:
             for record in training_data[training_type]["data"]:
-                value[training_type]["data"]+=int(record["value"])
+                value[training_type]["data"] += int(record["value"])
 
         for training_type in training_data:
             if training_type not in deliver_score_data['data']:
@@ -84,17 +98,18 @@ def submit_answers():
                 new_record = {
                     "id": new_id,
                     "value": "0",
-                    "date": current_date.strftime('%Y/%m/%d %H:%M:%S')
+                    "date": current_date.strftime('%Y/%m/%d %H:%M:%S'),
+                    "认知功能": cognitive_function
                 }
                 deliver_score_data['data'][training_type].append(new_record)
             else:
                 new_record = {
                     "id": new_id,
                     "value": str(value[training_type]["data"]),
-                    "date": current_date.strftime('%Y/%m/%d %H:%M:%S')
+                    "date": current_date.strftime('%Y/%m/%d %H:%M:%S'),
+                    "认知功能": cognitive_function
                 }
                 deliver_score_data['data'][training_type].append(new_record)
-            
 
         # 保存数据到deliver_score集合
         if deliver_score_collection.find_one({"username": user}):
